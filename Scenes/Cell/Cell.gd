e725c022c1f0ce783658aba55443f38d2b4a533a
Foica Serban <serban.foica@gmail.com>
2022-12-08 09:27:34 +0200

    Added randomizing algorithm stuff  - better randomizing algorithm  - change from cascades dict to array  - empty_board func  - add_to_cascades func

--- Scenes/Cell/Cell.gd
+++ Scenes/Cell/Cell.gd
@@ -1,7 +1,7 @@
 extends TextureButton
 
 func _ready():
-	var card: Utils.Card = Utils.Card.new(0)
+	var card := Utils.Card.new(0)
 	add_child(card.texture_button)
 	pass
 
--- Scripts/utils.gd
+++ Scripts/utils.gd
@@ -7,17 +7,8 @@ extends Node
 
 onready var DECK_SPRITESHEET = load("res://Assets/card_deck.tres")
 
-# cascade dict for memorizing card order for each cascade(column)
-var cascades: Dictionary = {
-	"casc1" : [],
-	"casc2" : [],
-	"casc3" : [],
-	"casc4" : [],
-	"casc5" : [],
-	"casc6" : [],
-	"casc7" : [],
-	"casc8" : []
-}
+# cascade Array of Arrays for memorizing card order for each cascade(column)
+var cascades: Array = [ [], [], [], [], [], [], [], []]
 
 class Card:
 	var id          # (from 0 to 51)
@@ -31,12 +22,64 @@ class Card:
 	func _init(id):
 		self.id = id
 		self.card_color = id / 13
-		self.card_number = id % 13\
+		self.card_number = id % 13
 		
 		# set texture for the button for the specific frame in the spritesheet
 		texture_button.texture_normal = Utils.DECK_SPRITESHEET.get_frame("default", id)
 		pass
 
 func _ready():
+	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		empty_board()
+		add_to_cascades(create_set())
+	pass
+
+#Cards: from 0 -> 51
+#4 sets - hearts (♥)
+#	   - diamonds (♦)
+#	   - spades (♠)
+#	   - clubs (♣)
+#	each 13 --> 52 cards in total
+
+#- num / 13 => card colour
+#- num % 13 => card number
+
+func create_set() -> Array:
+	# using randomize() to randomize the seed of the 
+	# internal random number generator
+	randomize()
 	
+	var card_id := []
+	
+	# creating the Array that will hold nums from 0 to 51
+	for i in range(52):
+		card_id.append(i)
+	
+	# randomizing the number array by picking random position and swapping between them
+	for i in range(52):
+		var poz: int = randi() % 52
+		# aux for swapping
+		var aux
+		aux = card_id[i]
+		card_id[i] = card_id[poz]
+		card_id[poz] = aux
+	
+	# YOU HAPPY NOW IOSUA?
+	return card_id
+	pass
+
+# add randomzied nums to cascades Array
+func add_to_cascades(ids: Array):
+	for i in range(52):
+		var casc = i % 8
+		cascades[casc].append(ids[i])
+	print(cascades)
+	pass
+
+func empty_board():
+	cascades.clear()
+	cascades.append_array([ [], [], [], [], [], [], [], [] ])
 	pass
