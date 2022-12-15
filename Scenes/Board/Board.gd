842a6ad679e9e7c7812ccadfc886c5612014d265
Foica Serban <serban.foica@gmail.com>
2022-12-15 09:05:52 +0200

    move board functions from utils to board script (im stoopid)

--- Scenes/Board/Board.gd
+++ Scenes/Board/Board.gd
@@ -1,14 +1,25 @@
 extends Control
 
+# cascade Array of Arrays for memorizing card order for each cascade(column)
+var casc_id: Array = [ [], [], [], [], [], [], [], [] ]
+
+# foundations Array of Arrays to hold solved cards
+var found_id: Array = [ [], [], [], [] ]
+
+# free_cells Array of 4 empty cells that will hold the temporary cards 
+var fc_id: Array = [52, 52, 52, 52]
+
 # --------------------------------- _ready() and _process(delta) ----------------------------------------
 
 func _ready():
+	# create postitions for all possible card objects
+	create_positions()
 	# create card objects and give them the corresponding positions with a blank sprite
 	# this is so we dont create 52 objects every game reset
 	for i in range(8):
 		for j in range(14):
 			var card = Utils.Card.new(52)
-			card.texture_button.set_position(Utils.casc_positions[i][j])
+			card.texture_button.set_position(casc_positions[i][j])
 			$Cards.add_child(card)
 			card.add_child(card.texture_button)
 			# add the card object to a cascade group that contains all cards in that cascade
@@ -23,27 +34,93 @@ func _process(delta):
 func _on_StartButton_pressed():
 	# on start empty the board and create a new randomized set
 	empty_board()
-	Utils.create_set()
+	create_set()
 	# give card objects respective ids
 	for i in range(52):
 		# card pos -> (i % 8, i / 8)
 		var card = get_tree().get_nodes_in_group("Cascade" + String(i % 8 + 1))[i / 8 + 1]
-		card.change_id(Utils.casc_id[i % 8][i / 8])
+		card.change_id(casc_id[i % 8][i / 8])
 		pass
 	pass 
 
 func empty_board():
-	Utils.casc_id.clear()
-	Utils.casc_id.append_array([ [], [], [], [], [], [], [], [] ])
+	casc_id.clear()
+	casc_id.append_array([ [], [], [], [], [], [], [], [] ])
 	
 	for i in range(8):
 		for j in range(14):
 			get_tree().get_nodes_in_group("Cascade" + String(i % 8 + 1))[j + 1].change_id(52)
 	
-	Utils.found_id.clear()
-	Utils.found_id.append_array([ [], [], [], [] ])
+	found_id.clear()
+	found_id.append_array([ [], [], [], [] ])
 	
-	Utils.fc_id.clear()
-	Utils.fc_id.append_array([ 52, 52, 52, 52])
+	fc_id.clear()
+	fc_id.append_array([ 52, 52, 52, 52])
 	
+	pass
+
+# ------------------------- RANDOMLY GENERATED SET ------------------------------------------------------------
+
+#Cards: from 0 -> 51
+#4 sets - hearts (♥) -> 0
+#	    - clubs (♣) -> 1
+#	    - diamonds (♦) -> 2
+#	    - spades (♠) -> 3
+#	each 13 --> 52 cards in total
+
+#- num / 13 => card colour
+#- num % 13 => card number
+
+func create_set():
+	# using randomize() to randomize the seed of the 
+	# internal random number generator
+	randomize()
+	
+	var card_id := []
+	
+	# creating the Array that will hold nums from 0 to 51
+	for i in range(52):
+		card_id.append(i)
+	
+	# randomizing the number array by picking random position
+	# and swapping between them
+	for i in range(52):
+		var poz: int = randi() % 52
+		# aux for swapping
+		var aux
+		aux = card_id[i]
+		card_id[i] = card_id[poz]
+		card_id[poz] = aux
+	
+	# YOU HAPPY NOW IOSUA?
+	
+	# adding the randomized array to the cascade_id array
+	for i in range(52):
+		var casc = i % 8
+		casc_id[casc].append(card_id[i])
+	pass
+
+# -------------------------------- CREATING POSITIONS ----------------------------------------------------------------
+
+# for cardpos2D positions:
+# - separately have positios for the free_cells, foundations and cascades
+# 
+# - top_left_pos var
+# - gap_x var for the gap between the cascades
+# - gap_y var for the gap between the cards in the cascades
+
+# card sprite dimensions = (33, 45)
+var casc_positions = [ [], [], [], [], [], [], [], [] ]
+
+var top_left_pos: Vector2 = Vector2(100, 100)
+var gap_x = 37
+var gap_y = 14
+
+func create_positions():
+	for i in range(8):
+		for j in range(18):
+			var pos: Vector2 = Vector2.ZERO
+			pos.x = top_left_pos.x + i * gap_x
+			pos.y = top_left_pos.y + j * gap_y
+			casc_positions[i].append(pos)
 	pass
--- Scripts/utils.gd
+++ Scripts/utils.gd
@@ -2,15 +2,6 @@ extends Node
 
 onready var DECK_SPRITESHEET = load("res://Assets/card_deck_bordered.tres")
 
-# cascade Array of Arrays for memorizing card order for each cascade(column)
-var casc_id: Array = [ [], [], [], [], [], [], [], [] ]
-
-# foundations Array of Arrays to hold solved cards
-var found_id: Array = [ [], [], [], [] ]
-
-# free_cells Array of 4 empty cells that will hold the temporary cards 
-var fc_id: Array = [52, 52, 52, 52]
-
 # --------------------------- CARD CLASS ---------------------------------------------------------------------------
 
 #CARD NOTATION:
@@ -25,6 +16,8 @@ class Card extends Node:
 	
 	# texture_button property for the card texture as well as clicking capabilities
 	var texture_button: TextureButton = TextureButton.new()
+	
+	# positions
 	
 	# Constructor
 	func _init(id):
@@ -41,91 +34,9 @@ class Card extends Node:
 		texture_button.texture_normal = Utils.DECK_SPRITESHEET.get_frame("default", id)
 		pass
 
-# ------------------------- RANDOMLY GENERATED SET ------------------------------------------------------------
-
-#Cards: from 0 -> 51
-#4 sets - hearts (♥)
-#	   - diamonds (♦)
-#	   - spades (♠)
-#	   - clubs (♣)
-#	each 13 --> 52 cards in total
-
-#- num / 13 => card colour
-#- num % 13 => card number
-
-func create_set():
-	# using randomize() to randomize the seed of the 
-	# internal random number generator
-	randomize()
-	
-	var card_id := []
-	
-	# creating the Array that will hold nums from 0 to 51
-	for i in range(52):
-		card_id.append(i)
-	
-	# randomizing the number array by picking random position
-	# and swapping between them
-	for i in range(52):
-		var poz: int = randi() % 52
-		# aux for swapping
-		var aux
-		aux = card_id[i]
-		card_id[i] = card_id[poz]
-		card_id[poz] = aux
-	
-	# YOU HAPPY NOW IOSUA?
-	
-	# adding the randomized array to the cascade_id array
-	for i in range(52):
-		var casc = i % 8
-		casc_id[casc].append(card_id[i])
-	pass
-
-# -------------------------------- CREATING POSITIONS ----------------------------------------------------------------
-
-# for cardpos2D positions:
-# - separately have positios for the free_cells and foundations
-# 
-# - top_left_pos var
-# - gap_x var for the gap between the cascades
-# - gap_y var for the gap between the cards in the cascades
-
-# card sprite dimensions = (33, 45)
-var casc_positions = [ [], [], [], [], [], [], [], [] ]
-
-var top_left_pos: Vector2 = Vector2(100, 100)
-var gap_x = 37
-var gap_y = 13
-
-func create_positions():
-	for i in range(8):
-		for j in range(14):
-			var pos: Vector2 = Vector2.ZERO
-			pos.x = top_left_pos.x + i * gap_x
-			pos.y = top_left_pos.y + j * gap_y
-			casc_positions[i].append(pos)
-	pass
-
-# -------------------------------- GAME START/ RESTART FUCNTIONS ----------------------------------------------
-
-# MOVED EMPTY_BOARD() TO BOARD SCRIPT
-
-#func empty_board():
-#	casc_id.clear()
-#	casc_id.append_array([ [], [], [], [], [], [], [], [] ])
-#
-#	found_id.clear()
-#	found_id.append_array([ [], [], [], [] ])
-#
-#	fc_id.clear()
-#	fc_id.append_array([ 52, 52, 52, 52])
-#	pass
-
 # --------------------------------- _ready() and _process(delta) ----------------------------------------
 
 func _ready():
-	create_positions()
 	pass
 
 func _process(delta):
